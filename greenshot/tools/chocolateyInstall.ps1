5856d1a321d7aa58431d36bdca13b4daae97f227
Robin <robin@getgreenshot.org>
2017-01-01 09:14:56 +0100

    Updated for 1.2.9.112

--- greenshot/tools/chocolateyInstall.ps1
+++ greenshot/tools/chocolateyInstall.ps1
@@ -1,2 +1,2 @@
 if (Get-Process 'Greenshot' -ea SilentlyContinue) {Stop-Process -processname Greenshot}
-Install-ChocolateyPackage 'greenshot' 'EXE' '/VERYSILENT' 'https://github.com/greenshot/greenshot/releases/download/Greenshot-RELEASE-1.2.9.104/Greenshot-INSTALLER-1.2.9.104-RELEASE.exe'
+Install-ChocolateyPackage 'greenshot' 'EXE' '/VERYSILENT' 'https://github.com/greenshot/greenshot/releases/download/Greenshot-RELEASE-1.2.9.112/Greenshot-INSTALLER-1.2.9.112-RELEASE.exe'
